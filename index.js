9102fbf11e19a4dc013bf35f3b6aad0c17684aeb
GABRIEL LUVIELMO XAVIER <gabriel_l_xavier@estudante.sesisenai.org.br>
2024-11-05 10:04:35 -0300

    Second Commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-//bibliote que vai gerenciar variáveis ambientes de um projeto
 require("dotenv").config();
+
+const db = require("./db");
 
 const port = process.env.PORT;
 
@@ -7,6 +8,14 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) =>{
+    res.json({message: "Funcionando"});
+})
+
+app.get("/clientes", async (req, res) => {
+    const clientes = await db.selectCustomers();
+    res.json(clientes);
+})
 app.listen(port);
 
-console.log("Backend Rodando");
+console.log("Backend rodando");
